a068a1c464e42fe51329b15e4185c7c983eadf40
Shriya Tripathi <73540883+shriya09-beep@users.noreply.github.com>
2024-01-31 15:08:18 +0530

    index.js

--- index.js
+++ index.js
@@ -91,39 +91,6 @@ function buttonAnimation(currentKey){
 
 
 
-//var soundList=["sounds/tom-1.mp3","sounds/tom-2.mp3","sounds/tom-3.mp3","sounds/tom-4.mp3","sounds/snare.mp3","sounds/crash.mp3","sounds/kick-bass.mp3"];
-//for(let i=0;i<document.querySelectorAll("button").length;i++){
-    
-//document.querySelectorAll("button")[i].addEventListener("click",function(){
-//var audio=new Audio(soundList[i]);
-//audio.play();
-//});
-//}
-
-//document.addEventListener("keydown", function(){
-  //  alert("key was pressed!");
-//});
-
-
-
-
-
-
-
-
-
-
-
-
-
-//document.querySelector(".set").addEventListener("click", handleClick);
-
-//function handleClick(){
-    //var audio =new Audio("sounds/tom-1.mp3");
-    //audio.play();
-    //alert("I got clicked");
-//}
-
 
 
 
